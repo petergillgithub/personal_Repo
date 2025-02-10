4a8ba8b3940b00b73bec5ce3e4756ec49a0204f4
petergillgithub <petergillhmg@gmail.com>
2025-02-10 20:19:55 +0000

    Fetch Data from an API

---
+++ #48  Fetch data from an API /app.js
@@ -0,0 +1,6 @@
+// fetch = Function used for making HTTP requests to fetch resources.
+//         (JSON style data, images,files)
+//         Simplifies asynchronous data fetching in JavaScript and used for interacting with APIs to retreive and send data asynchronously over the web.
+//        fetch(url,{options})
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
