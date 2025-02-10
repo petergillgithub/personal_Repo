5c8c0a4a99d3f06e6789cd52d121cb0c68dbb468
petergillgithub <petergillhmg@gmail.com>
2025-02-10 20:08:25 +0000

    DateObject

---
+++ #29  dateobjects()/app.js
@@ -0,0 +1,18 @@
+// Date objects = Objects that contain values that represent dates and times.
+//                These date objects can be changed anddd formatted..
+// Date (year,month,day,hour,minute,second,ms)
+
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay()
+const hour = date.getHours();
+const minutes = date.getMinutes()
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+
