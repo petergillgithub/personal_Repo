de04bb72698a2ae00763587916cebb21f48f5cd5
petergillgithub <petergillhmg@gmail.com>
2025-02-10 19:50:24 +0000

    Array

---
+++ #2 Array/app.js
@@ -0,0 +1,17 @@
+/* let colors;
+colors = ['white','black','green'];
+
+let elcolor = document.getElementById('color');
+elcolor.textContent = colors[2]; */
+
+// Example : 2
+/* 
+let colors = ['pink', 'green', 'orange'];
+
+colors[0] = 'yellow' // Value changed pink to yellow
+
+let elcolor = document.getElementById('color');
+elcolor.textContent = colors[0] */
+
+// ********************************************
+
