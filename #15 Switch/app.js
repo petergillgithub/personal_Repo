d27605137aa96a6fbb5cf12349bdad2178cbfa33
petergillgithub <petergillhmg@gmail.com>
2025-02-10 20:01:06 +0000

    Switch

---
+++ #15 Switch/app.js
@@ -0,0 +1,84 @@
+// Switch = can be an efficient replacement to many else if statement
+
+/* let day = 100;
+
+if (day == 1){
+  console.log('It is monday');
+}
+else if (day == 2){
+  console.log('It is Tuesday');
+}
+else if (day == 3){
+  console.log('It is Wednesday');
+}
+else if (day == 4){
+  console.log('It is Thursday');
+}
+else if (day == 5){
+  console.log('It is Friday');
+}
+else if (day == 6){
+  console.log('It is Saturday');
+}
+else if (day == 7){
+  console.log('It is Sunday');
+}
+else{
+  console.log(` ${day} is not correct statement`);
+} */
+
+
+/// Another way
+
+/* let day = 3;
+let msg;
+
+switch (day) {
+  case 1:
+    console.log("It's Monday");
+    break
+  case 2:
+    console.log("It's Tuesday");
+    break
+  case 3:
+    console.log("It's Wednesday");
+    break
+  case 4:
+    console.log("It's Thursday");
+    break
+  case 5:
+    console.log("It's Friday");
+    break
+  case 6:
+    console.log("It's Saturday");
+    break
+  case 7:
+    console.log("It's Sunday");
+    break
+  default: {
+    console.log(`${day} is not day`);
+  }
+} */
+
+// let testScore = 92;
+// let letterGrade;
+
+// switch (true) {
+//   case testScore >= 90:
+//     letterGrade = "A";
+//     break;
+//   case testScore >= 80:
+//     letterGrade = "B";
+//     break;
+//   case testScore >= 70:
+//     letterGrade = "C";
+//     break;
+//   case testScore >= 60:
+//     letterGrade = "D";
+//     break;
+//   default:
+//     letterGrade = "F"
+
+// }
+
+// console.log(letterGrade);
