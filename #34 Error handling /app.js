b5fad3ab1933aa7bc05d9a87ab711c6c1dc7023f
petergillgithub <petergillhmg@gmail.com>
2025-02-10 20:10:50 +0000

    Error Handling

---
+++ #34 Error handling /app.js
@@ -0,0 +1,7 @@
+// Error - An Object that is created to represent a problem that occurs Occur often with user input or establishing or establishing a connection..
+
+
+// try { } = Encloses code that might potentially cause an error..
+// catch { } = Catch and handle any thrown Errors from try { }
+//finally {  } = (optional) always executes,Used mostly for clean up ex. close files,close connections,release resources.
+
