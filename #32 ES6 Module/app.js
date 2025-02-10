3c779aba8eb09be7a5130b92bb327be01f19920b
petergillgithub <petergillhmg@gmail.com>
2025-02-10 20:09:48 +0000

    ES6 Module

---
+++ #32 ES6 Module/app.js
@@ -0,0 +1,8 @@
+// ES6 Module = An external file that contains reusable code that can be improved into oher javascript files.
+// Write reusable code for many different apps..
+// can contain variables, classes,functions ... and more Introduced as part of ECMAScript 2015 update...
+
+import {add,subtract} from './math.js';
+
+console.log(add(2,10));
+console.log(subtract(10,6));
